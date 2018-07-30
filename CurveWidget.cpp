1eb232e0c4aa45e5f631d515f9795e7220cb88bd
Konstantin <consta@unigine.com>
2018-07-31 01:17:45 +0800

    3

--- CurveWidget.cpp
+++ CurveWidget.cpp
@@ -21,31 +21,56 @@ void CurveWidget::paintEvent(QPaintEvent *e)
 	QPainter painter(this);
 	painter.setRenderHint(QPainter::Antialiasing, true);
 
+	painter.setRenderHint(QPainter::Antialiasing, false);
+	int gridWidth = 1;
+
+
+	//
+	// Thin grid lines
+	//
+	QColor gridColorThin(46, 46, 46);
+	painter.setPen(QPen(gridColorThin, gridWidth, Qt::SolidLine, Qt::FlatCap));
+	for (int i = 1; i <= size().width() * 10 / getLinesUnit(); i++)
+	{
+		painter.drawLine(i * getLinesUnit() / 10, 0, i * getLinesUnit() / 10, size().height());
+	}
+	painter.setPen(QPen(gridColorThin, gridWidth, Qt::SolidLine, Qt::FlatCap));
+	for (int i = 1; i <= size().height() * 10 / getLinesUnit(); i++)
+	{
+		painter.drawLine(0, i * getLinesUnit() / 10, size().width(), i * getLinesUnit() / 10);
+	}
+
+
+	//
+	// Fat grid lines
+	//
+	QColor gridColorFat(90, 90, 90);
+	painter.setPen(QPen(gridColorFat, gridWidth, Qt::SolidLine, Qt::FlatCap));
+	for (int i = 1; i <= size().width() / getLinesUnit(); i++)
+	{
+		painter.drawLine(i * getLinesUnit(), 0, i * getLinesUnit(), size().height());
+	}
+	painter.setPen(QPen(gridColorFat, gridWidth, Qt::SolidLine, Qt::FlatCap));
+	for (int i = 1; i <= size().height() / getLinesUnit(); i++)
+	{
+		painter.drawLine(0, i * getLinesUnit(), size().width(), i * getLinesUnit());
+	}
+
+
 	/* Make the Gradient for this line. */
 	//const QPointF start{ 0,0 };
 	//const QPointF end{ 100,100 };
 	//QLinearGradient gradient(start, end);
 	//QColor color(123, 123, 123); //some color
-	//color.setAlphaF(0.9); //change alpha
+	//color.setAlphaF(0.1); //change alpha
 	//gradient.setColorAt(0, color);
 	//color.setAlphaF(0.1); //change alpha again
 	//gradient.setColorAt(1, color );
-	painter.setRenderHint(QPainter::Antialiasing, false);
-	QColor gridColor(90, 90, 90);
-	int gridWidth = 1;
 
-	for (int i = 1; i <= size().width() / getLinesUnit(); i++)
-	{
-		painter.setPen(QPen(gridColor, gridWidth, Qt::SolidLine, Qt::FlatCap));
-		painter.drawLine(i * getLinesUnit(), 0, i * getLinesUnit(), size().height());
-	}
 
-	for (int i = 1; i <= size().height() / getLinesUnit(); i++)
-	{
-		painter.setPen(QPen(gridColor, gridWidth, Qt::SolidLine, Qt::FlatCap));
-		painter.drawLine(0, i * getLinesUnit(), size().width(), i * getLinesUnit());
-	}
-
+	//
+	// Curves
+	//
 	painter.setPen(QPen(Qt::green, 2, Qt::SolidLine, Qt::RoundCap));
 	painter.drawLine(ToCanvasCoordinates(QPoint(0,0)), ToCanvasCoordinates(QPoint(getLinesUnit(), getLinesUnit())));
 
